16ae114d32a181dec6b4f605e5c65ba76325d58e
Yasuhiro Kawano <y-kawano@ceres-inc.jp>
2019-07-26 12:10:03 +0900

    modify comment

--- src/lesson_1.php
+++ src/lesson_1.php
@@ -12,46 +12,58 @@ $driver = RemoteWebDriver::create($host, DesiredCapabilities::chrome());
 
 // テスト用サイト(予約フォーム)にアクセスする
 $driver->get('http://example.selenium.jp/reserveApp/');
-
 sleep(3);
 
 
-// 宿泊日を設定する (各テキストボックスに「2019」年「8」月「1」日から「3」泊を入力する)
+/**
+ * 宿泊日を設定する (各テキストボックスに「2019」年「8」月「1」日から「3」泊を入力する)
+ */
 $driver->findElement(WebDriverBy::name('reserve_y'))->clear()->sendKeys(2019);
 $driver->findElement(WebDriverBy::name('reserve_m'))->clear()->sendKeys(8);
 $driver->findElement(WebDriverBy::name('reserve_d'))->clear()->sendKeys(1);
 $driver->findElement(WebDriverBy::name('reserve_t'))->clear()->sendKeys(3);
 
 
-// 人数を設定する (テキストボックスに「3」人を入力する)
+/**
+ * 人数を設定する (テキストボックスに「3」人を入力する)
+ */
 $driver->findElement(WebDriverBy::name('hc'))->clear()->sendKeys(3);
 
 
-// 朝食バイキングを設定する (ラジオボタンの「なし」を選択する)
+/**
+ * 朝食バイキングを設定する (ラジオボタンの「なし」を選択する)
+ */
 $driver->findElement(WebDriverBy::id('breakfast_off'))->click();
 
 
-// プランを設定する(チェックボックスの「昼からチェックインプラン」を選択する)
+/**
+ * プランを設定する(チェックボックスの「昼からチェックインプラン」を選択する)
+ */
 $driver->findElement(WebDriverBy::name('plan_a'))->click();
 
 
-// お名前を設定する (テキストボックスに「hoge」を入力する)
+/** 
+ * お名前を設定する (テキストボックスに「hoge」を入力する)
+ */
 $driver->findElement(WebDriverBy::name('gname'))->clear()->sendKeys('hoge');
-
 sleep(3);
 
 
-// 次へをクリックする
+/**
+ * 次へをクリックする
+ */
 $driver->findElement(WebDriverBy::id('goto_next'))->click();
-
 sleep(3);
 
 
-// 確定をクリックする
+/**
+ * 確定をクリックする
+ */
 $driver->findElement(WebDriverBy::id('commit'))->click();
-
 sleep(3);
 
 
-// ウィンドウを閉じる
+/**
+ * ウィンドウを閉じる
+ */
 $driver->quit();
